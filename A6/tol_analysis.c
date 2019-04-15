ac2b164c7f9abe34016d8527db94e07d8b275e9b
SilasA <agnews@eos03.cis.gvsu.edu>
2019-04-15 14:50:23 -0400

    Just stag 3 to go

--- A6/tol_analysis.c
+++ A6/tol_analysis.c
@@ -31,19 +31,19 @@ struct dimension_t {
 typedef struct dimension_t dimension_t;
 
 dimension_t _dimensions[15];
-int _size;
+int _size = 0;
 
 dimension_t *_gapptr;
 
 dimension_t create_part(int part, float nominal, float direction, float tolerance, bool fixed);
 dimension_t create_gap(int gap, float upper_tol, float lower_tol);
 void populate_dimensions(FILE *file);
-void print_dimensions();
+void print_dimensions(bool gaps);
 
 float calc_gap_mean();
 float calc_gap_tol();
-void calc_new_dim(dimension_t *gap);
-void calc_new_tol(dimension_t *gap);
+void calc_new_dim();
+void calc_new_tol();
 
 int main(int argc, char **argv)
 {
@@ -56,7 +56,7 @@ int main(int argc, char **argv)
     }
 
     populate_dimensions(file);
-    //print_dimensions();
+    //print_dimensions(true);
         
     float gapMean = calc_gap_mean();
     float gapTol = calc_gap_tol();
@@ -84,21 +84,24 @@ int main(int argc, char **argv)
     else
         minEquality = "Equals";
 
-    printf("Actual Gap Mean: %g\"", gapMean);
-    printf("Actual Gap Tolerance: %g\"", gapTol);
-    printf("The Maximum Gap (%g\") is (%s) than specified (%g\")", maxGap, maxEquality, gapUpperTol);
-    printf("The Minimum Gap (%g\") is (%s) than the specified (%g\")", minGap, minEquality, gapLowerTol);
+    printf("Actual Gap Mean: %g\"\n", gapMean);
+    printf("Actual Gap Tolerance: %g\"\n", gapTol);
+    printf("The Maximum Gap (%g\") is (%s) than specified (%g\")\n", maxGap, maxEquality, gapUpperTol);
+    printf("The Minimum Gap (%g\") is (%s) than the specified (%g\")\n\n", minGap, minEquality, gapLowerTol);
 
-    printf("Recommended Adjustments to meet GAP %g, %g:\n", _gapptr->lower_tol, _gapptr->upper_tol);
-    
+    printf("Recommended Adjustments to meet GAP %g, %g:\n", gapLowerTol, gapUpperTol);
+    calc_new_dim();
+    calc_new_tol();
+    print_dimensions(false);
     return 0;
 }
 
 dimension_t create_part(int part, float nominal, float direction, float tolerance, bool fixed)
 {
-    dimension_t d;;
+    dimension_t d;
     d.type = part;
     d.nominal = nominal;
+    d.direction = direction;
     d.upper_tol = tolerance;
     d.lower_tol = -tolerance;
     d.fixed = fixed;
@@ -128,11 +131,11 @@ void populate_dimensions(FILE *file)
             tokens[i] = token;
             token = strtok(NULL, ",");
         } 
-    
+
         dimension_t d;
         if (strcmp(tokens[0], "PART") == 0)
         {
-            d = create_part(PART,
+           d = create_part(PART,
                     strtof(tokens[1], NULL),
                     strtof(tokens[2], NULL),
                     strtof(tokens[3], NULL),
@@ -145,22 +148,23 @@ void populate_dimensions(FILE *file)
                     strtof(tokens[1], NULL),
                     strtof(tokens[2], NULL));
             _dimensions[_size++] = d;
-            _gapptr = _dimensions + (_size - 1);
+            _gapptr = &(_dimensions[_size - 1]);
         }
     }
+    free(line);
 }
 
-void print_dimensions()
+void print_dimensions(bool gaps)
 {
     for (int i = 0; i < _size; i++)
     {
         if (_dimensions[i].type == PART)
         {
             printf("PART--Nom: %6g Direct: %6g Tol: %6g Fixed: %s\n",
-                    _dimensions[i].nominal, _dimensions[i].lower_tol, _dimensions[i].upper_tol,
+                    _dimensions[i].nominal, _dimensions[i].direction, _dimensions[i].upper_tol,
                     _dimensions[i].fixed ? "true" : "false");
         }
-        else
+        else if (gaps)
         {
             printf("GAP---Min: %6g Max: %6g\n", _dimensions[i].lower_tol,
                     _dimensions[i].upper_tol);
@@ -180,13 +184,13 @@ float calc_gap_mean()
             count++;
         }
     }
-    return sum / count;
+    return sum;
 }
 
 float calc_gap_tol()
 {
     float sum = 0;
-    for (int i = 0; i < _size++; i++)
+    for (int i = 0; i < _size; i++)
     {
         if (_dimensions[i].type == PART)
         {
@@ -196,7 +200,7 @@ float calc_gap_tol()
     return sum;
 };
 
-void calc_new_dim(dimension_t *gap)
+void calc_new_dim()
 {
     float scale;
     float sum;
@@ -217,7 +221,7 @@ void calc_new_dim(dimension_t *gap)
         }
     }
     
-    scale = sum / nom_gap;
+    scale = nom_gap / sum;
     
     for (int i = 0; i < _size; i++)
     {
@@ -231,13 +235,13 @@ void calc_new_dim(dimension_t *gap)
     }
 }
 
-void calc_new_tol(dimension_t *gap)
+void calc_new_tol()
 {
     float scale;
-    float sum;
+    float sum = 0;
     float fixed;
     float nom_gap = (_gapptr->upper_tol + _gapptr->lower_tol) / 2.0;
-    
+
     for (int i = 0; i < _size; i++)
     {
         if (_dimensions[i].type != GAP)
@@ -251,9 +255,9 @@ void calc_new_tol(dimension_t *gap)
                 sum += _dimensions[i].upper_tol;
         }
     }
-    
-    scale = sum / nom_gap;
-    
+
+    scale = nom_gap / sum;
+
     for (int i = 0; i < _size; i++)
     {
         if (_dimensions[i].type != GAP)
